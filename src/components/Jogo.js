a63c01da50a06fbbf6a0da44a9d88d6ea3ba1a40
carlmnm <carlossmiguell3@gmail.com>
2022-11-27 01:54:39 -0300

    a palavra fica escondida com underline no lugar das letras

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import forca0 from "../assets/img/forca0.png"
 import forca1 from "../assets/img/forca1.png"
 import forca2 from "../assets/img/forca2.png"
@@ -7,19 +8,52 @@ import forca5 from "../assets/img/forca5.png"
 import forca6 from "../assets/img/forca6.png"
 import Letras from "./Letras"
 import Chute from "./chute"
+import palavras from "./palavras"
+let escondida = []
 
 export default function Jogo() {
+    let [palavraSelecionada, setPalavraSelecionada] = useState("")
+    let [arrayPalavra, setArrayPalavra] = useState([])
+    console.log(palavraSelecionada)
+
+    function selecionaPalavra(props) {
+        const numeroAleatorio = parseInt(Math.random() * palavras.length)
+        const palavraAleatoria = palavras[numeroAleatorio]
+
+        setPalavraSelecionada(palavraAleatoria)
+        setArrayPalavra(palavraSelecionada.split(''))
+
+        while(escondida.length){
+            escondida.pop()
+        }
+        for(let i = 0; i < palavraAleatoria.length; i++){
+            escondida[i] = '_ '
+        }
+        
+        return (
+            <span className="letra">
+                {props.caractere}
+            </span>
+        )
+    }
+
     return (
         <div className="container-jogo">
             <div className="forca-imgs">
                 <img src={forca0} alt="imagem da forca" className="forca" />
             </div>
-            <button className="botao-escolher-palavra" type="button">Escolher Palavra</button>
+            <button className="botao-escolher-palavra" onClick={selecionaPalavra} type="button">Escolher Palavra</button>
 
             <Letras />
-            
+
             <div className="container-chute">
                 <Chute />
+            </div>
+
+            <div className="container-palavra">
+                <span className="palavra-jogo">
+                    {escondida}
+                </span>
             </div>
 
         </div>
